24eb71adedfb97082ed0e88ba3ec4a27af629eca
hanfenglei KF6425 (RD) <hkf6425@h3c.huawei-3com.com>
2020-07-31 17:25:55 +0800

    add

--- module/data_structures/linked_list/index.js
+++ module/data_structures/linked_list/index.js
@@ -7,33 +7,98 @@ class Node {
 
 class LinkedList {
   constructor() {
-    this.head = new Node('head')
+    this.head = null
+    this.length = 0
   }
 
-  find (item) {
+  getElementAt (position) {
+    if (position < 0 || position >= this.length) {
+      return null
+    }
+
+    let current = this.head
+    for (let i = 0; i < position; i++) {
+      current = current.next
+    }
+
+    return current
+  }
+
+  append (element) {
+    let node = new Node(element)
+
+    if (this.head === null) {
+      this.head = node
+    }
+    else {
+      let current = this.getElementAt(this.length - 1)
+
+      current.next = node
+    }
+
+    this.length++
+  }
+
+  insertAt (position, element) {
+    if (position < 0 || position > this.length) {
+      return false
+    }
+
+    let node = new Node(element)
+
+    if (position === 0) {
+      node.next = this.head
+      this.head = node
+    }
+    else {
+      let previous = this.getElementAt(position - 1)
+      node.next = previous.next
+      previous.next = node
+    }
+
+    this.length++
+    return true
+  }
+
+  /* get element index */
+  indexOf (element) {
+    let current = this.head
+
+    for (let i = 0; i < this.length; i++) {
+      if (current.element === element) {
+        return i
+      }
+
+      current = current.next
+    }
+
+    return -1
+  }
+
+  find (element) {
     let currNode = this.head
 
-    while (currNode.element && (currNode.element != item)) {
+    while (currNode.element && (currNode.element != element)) {
       currNode = currNode.next
     }
 
     return currNode
   }
 
-  findPrev (item) {
+  findPrev (element) {
     let currNode = this.head
 
-    while (currNode.next !== null && (currNode.next.element != item)) {
+    while (currNode.next !== null && (currNode.next.element != element)) {
       currNode = currNode.next
     }
 
     return currNode
   }
 
-  afterInsertSpecialElement (newElement, item) {
+  afterInsertSpecialElement (newElement, element) {
     let newNode = new Node(newElement)
 
-    let currNode = this.find(item)
+    let currNode = this.find(element)
 
     newNode.next = currNode.next
     currNode.next = newNode
@@ -41,21 +106,46 @@ class LinkedList {
     return null
   }
 
-  remove (item) {
-    let prevNode = this.findPrev(item)
+  remove (element) {
+    // 方法1:
+    let index = this.indexOf(element)
+    return this.removeAt(index)
 
-    if (prevNode.next && prevNode.next.element != "head") {
-      prevNode.next.element = null
-    }
+    // 方法2:
+    // let prevNode = this.findPrev(element)
 
-    if (prevNode.next !== null) {
-      prevNode.next = prevNode.next.next
-    }
+    // if (prevNode.next && prevNode.next.element != "head") {
+    //   prevNode.next.element = null
+    // }
 
-    return null
+    // if (prevNode.next !== null) {
+    //   prevNode.next = prevNode.next.next
+    // }
+
+    // return null
   }
 
-  display () {
+  removeAt (position) {
+    if (position < 0 || position > this.length) {
+      return false
+    }
+
+    let current = this.head
+
+    if (position === 0) {
+      this.head = current.next
+    }
+    else {
+      let previous = this.getElementAt(position - 1)
+      current = previous.next
+      previous.next = current.next
+    }
+
+    this.length--
+    return current.element
+  }
+
+  getAllElementByArray () {
     let res = []
     let currNode = this.head
 
@@ -65,6 +155,39 @@ class LinkedList {
     }
 
     return res
+  }
+
+  display () {
+    let current = this.head
+    let s = ''
+
+    while (current) {
+      let next = current.next
+
+      next = next ? next.element : 'null'
+      s += `[element: ${current.element}, next: ${next}]\n`
+      current = current.next
+    }
+
+    return s
+  }
+
+  isEmpty () {
+    // return this.head === null
+    return this.length === 0
+  }
+
+  size () {
+    return this.length
+  }
+
+  getHead () {
+    return this.head
+  }
+
+  clear () {
+    this.head = null
+    this.length = 0
   }
 }
 
--- module/data_structures/linked_list/test.js
+++ module/data_structures/linked_list/test.js
@@ -1,11 +1,26 @@
-const list = require('./index.js')
+const linkedList = require('./index.js')
 
-list.insert('Apple', 'head')
-list.insert('Banana', 'Apple')
-list.insert('Pear', 'Banana')
+linkedList.append(10)
+linkedList.append(15)
+linkedList.append(20)
 
-console.log(list.display())
+console.log(linkedList.display())
 
+linkedList.insertAt(0, 9)
+linkedList.insertAt(2, 11)
+linkedList.insertAt(5, 25)
+console.log(linkedList.display())
 
-list.remove('Banana')
-console.log(list.display())
+console.log(linkedList.removeAt(0))
+console.log(linkedList.removeAt(1))
+console.log(linkedList.removeAt(3))
+console.log(linkedList.display())
+
+console.log(linkedList.indexOf(20))
+
+linkedList.remove(20)
+
+console.log(linkedList.display())
+
+linkedList.clear()
+console.log(linkedList.size())
