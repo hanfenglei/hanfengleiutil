060bc39963dca4e577aa7399a615666e164920c3
hanfenglei KF6425 (RD) <hkf6425@h3c.huawei-3com.com>
2020-07-31 16:19:45 +0800

    123

--- test/index.js
+++
@@ -1,8 +0,0 @@
-// const timeMod = require('../module/time.js')
-
-// console.log(timeMod.stampThisHour(5))
-// const moment = require('moment')
-// console.log(moment().day())
-// console.log(moment().day())
-// console.log(moment().days())
-// console.log(moment().days())
